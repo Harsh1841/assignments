8a109d30e89c2c42f9b82c91624ebf42d318314d
Harsh1841 <harsh1841.be23@chitkara.edu.in>
2025-07-19 16:25:10 +0530

    done

---
+++ RecipeLog/RecipeLogTests/RecipeLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeLogTests.swift
+//  RecipeLogTests
+//
+//  Created by Student on 19/07/25.
+//
+
+import Testing
+@testable import RecipeLog
+
+struct RecipeLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
